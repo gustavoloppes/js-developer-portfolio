4fbd1f808cfe644c725494101a1d285f9c62f42c
gustavo lopes <glopes0712@gmail.com>
2023-06-21 19:26:45 -0400

    Atualizacao de link de consumo do arquivo JSON

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://github.dev/gustavoloppes/js-developer-portfolio/blob/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/gustavoloppes/js-developer-portfolio/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
